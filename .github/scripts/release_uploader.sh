e1e78cde9437320187df4c80a5985f3b482776dc
Alexander N <435658+anein@users.noreply.github.com>
2019-09-30 21:07:57 +0300

    Update release_uploader.sh

--- .github/scripts/release_uploader.sh
+++ .github/scripts/release_uploader.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-
-function request() {
-    response=$(curl "$@" \
-                    -w '\n%{http_code}\n' \
-                    -s \
-                    -sSL \
-                    -X POST \
-                    -H "${AUTH_HEADER}" \
-              )
-
-    readarray -t tuple <<<"$response"
-
-    message=$(echo "${tuple[0]}" | jq '.message')
-    status="${tuple[1]}"
-
-    echo "Status: ${status}"
-    echo "Body: ${message}"
-
-    if [ "$status" -ge 400 ]; then
-      echo ::error::"${message}"
-      exit;
-    fi
-}
+#
+#function request() {
+#    response=$(curl "$@" \
+#                    -w '\n%{http_code}\n' \
+#                    -s \
+#                    -sSL \
+#                    -X POST \
+#                    -H "${AUTH_HEADER}" \
+#              )
+#
+#    readarray -t tuple <<<"$response"
+#
+#    message=$(echo "${tuple[0]}" | jq '.message')
+#    status="${tuple[1]}"
+#
+#    echo "Status: ${status}"
+#    echo "Body: ${message}"
+#
+#    if [ "$status" -ge 400 ]; then
+#      echo ::error::"${message}"
+#      exit;
+#    fi
+#}
 
 
 if [ -z "$TAG_VERSION" ]; then
@@ -46,8 +46,35 @@ RELEASE_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
 UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${TAG_VERSION}/assets?name=${FILENAME}"
 
-echo "$RELEASE_URL"
 
-#Create release
-request "${RELEASE_URL}" \
-        -d "{'tag_version': '${TAG_VERSION}', 'name': 'v${TAG_VERSION}', 'body': 'Description'}"
+response=$(curl   -d "{'tag_version': '${TAG_VERSION}', 'name': 'v${TAG_VERSION}', 'body': 'Description'}" \
+                  -w '\n%{http_code}\n' \
+                  -s \
+                  -sSL \
+                  -X POST \
+                  -H "${AUTH_HEADER}" \
+                  "${RELEASE_URL}"
+              )
+
+readarray -t tuple <<<"$response"
+
+message=$(echo "${tuple[0]}" | jq '.message')
+status="${tuple[1]}"
+
+echo "Status: ${status}"
+echo "Body: ${message}"
+
+if [ "$status" -ge 400 ]; then
+      echo ::error::"${message}"
+      exit;
+fi
+
+# Upload the file
+curl \
+  -sSL \
+  -XPOST \
+  -H "${AUTH_HEADER}" \
+  -H "${CONTENT_LENGTH_HEADER}" \
+  -H "${CONTENT_TYPE_HEADER}" \
+  --data-binary @FILENAME \
+  "${UPLOAD_URL}"
