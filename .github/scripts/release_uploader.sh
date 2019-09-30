7169d51fe5c8b7cc01c03f92fcca16bad8b50706
Alexander N <435658+anein@users.noreply.github.com>
2019-09-30 21:03:40 +0300

    Update release_uploader.sh

--- .github/scripts/release_uploader.sh
+++ .github/scripts/release_uploader.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo $TAG_VERSION;
+function request() {
+    response=$(curl "$@" \
+                    -w '\n%{http_code}\n' \
+                    -s \
+                    -sSL \
+                    -X POST \
+                    -H "${AUTH_HEADER}" \
+              )
+
+    readarray -t tuple <<<"$response"
+
+    message=$(echo "${tuple[0]}" | jq '.message')
+    status="${tuple[1]}"
+
+    echo "Status: ${status}"
+    echo "Body: ${message}"
+
+    if [ "$status" -ge 400 ]; then
+      echo ::error::"${message}"
+      exit;
+    fi
+}
+
 
 if [ -z "$TAG_VERSION" ]; then
-  echo ::error "Variable TAG_VERSION is empty"
+  echo ::error::"Variable TAG_VERSION is empty"
   exit
 fi
 
@@ -11,7 +33,7 @@ FILENAME=twitter_counter.$TAG_VERSION.zip
 
 # Check if the release file exists
 if [ ! -f "$FILENAME" ]; then
-  echo ::error "File $FILENAME doesn't exist"
+  echo ::error::"File $FILENAME doesn't exist"
   exist
 fi
 
@@ -25,20 +47,7 @@ RELEASE_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${TAG_VERSION}/assets?name=${FILENAME}"
 
 echo "$RELEASE_URL"
-#Create release
-curl \
-  -sSL \
-  -XPOST \
-  -d "{'tag_version': '$TAG_VERSION', 'name': 'v$TAG_VERSION', 'body': 'Description'}" \
-  -H "${AUTH_HEADER}" \
-  "${RELEASE_URL}"
 
-# Upload the file
-curl \
-  -sSL \
-  -XPOST \
-  -H "${AUTH_HEADER}" \
-  -H "${CONTENT_LENGTH_HEADER}" \
-  -H "${CONTENT_TYPE_HEADER}" \
-  --data-binary @FILENAME \
-  "${UPLOAD_URL}"
+#Create release
+request "${RELEASE_URL}" \
+        -d "{'tag_version': '${TAG_VERSION}', 'name': 'v${TAG_VERSION}', 'body': 'Description'}"
