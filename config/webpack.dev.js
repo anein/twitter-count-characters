a190ce65dfb7642d3e9bb0b811ac31a0e5032c77
Alexander Ein <435658+anein@users.noreply.github.com>
2019-10-09 14:12:52 +0300

    chore: remove extension reloader plugin from prod

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,79 +3,85 @@ const plugins = require('./helpers.plugins');
 const configCommon = require('./webpack.common');
 //
 
-const configScripts = {
-  name: 'Config Content',
-  entry: {
-    tweetdeck: ['./ts/tweetdeck.ts'],
-    web: ['./ts/web.ts'],
-    'scripts/web': ['./ts/scripts/web.ts'],
-  },
+const configContentScripts = env => {
+  const isProduction = !!(env && 'production' in env && env.production === true);
 
-  output: {
-    path: helpers.root('build/'),
-    filename: 'js/[name].js',
-  },
+  return {
+    name: 'Config Content',
+    entry: {
+      tweetdeck: ['./ts/tweetdeck.ts'],
+      web: ['./ts/web.ts'],
+      'scripts/web': ['./ts/scripts/web.ts'],
+    },
 
-  plugins: [new plugins.ExtensionReloader()],
+    output: {
+      path: helpers.root('build/'),
+      filename: 'js/[name].js',
+    },
+
+    plugins: [isProduction ? () => {} : new plugins.ExtensionReloader()],
+  };
 };
 
-const configPopupAndAssets = {
-  name: 'Config Popup and Assets',
-  entry: {
-    popup: ['./ts/popup.ts', './css/popup.css'],
-    content: ['./css/content.css'],
-  },
+const configPopupAndAssets = env => {
+  return {
+    name: 'Config Popup and Assets',
+    entry: {
+      popup: ['./ts/popup.ts', './css/popup.css'],
+      content: ['./css/content.css'],
+    },
 
-  output: {
-    filename: 'js/[name].js',
-    path: helpers.root('build'),
-  },
+    output: {
+      filename: 'js/[name].js',
+      path: helpers.root('build'),
+    },
 
-  plugins: [
-    new plugins.MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
-    new plugins.HtmlWebpackPlugin({
-      filename: 'html/popup.html',
-      template: './html/popup.html',
-      inject: false,
-      minify: {
-        removeAttributeQuotes: true,
-        collapseWhitespace: true,
-        html5: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-      },
-    }),
-    /**
-     * Plugin: CopyWebpackPlugin
-     * Description: Copy files and directories in webpack.
-     */
-    new plugins.CopyWebpackPlugin(
-      [
-        {
-          from: 'manifest.json',
+    plugins: [
+      new plugins.MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+      new plugins.HtmlWebpackPlugin({
+        filename: 'html/popup.html',
+        template: './html/popup.html',
+        inject: false,
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          html5: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
         },
+      }),
+      /**
+       * Plugin: CopyWebpackPlugin
+       * Description: Copy files and directories in webpack.
+       */
+      new plugins.CopyWebpackPlugin(
+        [
+          {
+            from: 'manifest.json',
+          },
+          {
+            from: 'img',
+            to: 'img',
+          },
+          {
+            from: '../_locales',
+            to: '_locales',
+          },
+        ],
         {
-          from: 'img',
-          to: 'img',
-        },
-        {
-          from: '../_locales',
-          to: '_locales',
-        },
-      ],
-      {
-        // By default, we only copy modified files during
-        // a watch or webpack-dev-server build. Setting this
-        // to `true` copies all files.
-        copyUnmodified: true,
-      }
-    ),
-  ],
+          // By default, we only copy modified files during
+          // a watch or webpack-dev-server build. Setting this
+          // to `true` copies all files.
+          copyUnmodified: true,
+        }
+      ),
+    ],
+  };
 };
 
 // combine all configurations and entries.
 module.exports = env => {
-  return [configScripts, configPopupAndAssets].map(entry => plugins.webpackMerge([configCommon(env), entry]));
+  return [configContentScripts, configPopupAndAssets].map(entry => plugins.webpackMerge([configCommon(env), entry(env)]));
 };
